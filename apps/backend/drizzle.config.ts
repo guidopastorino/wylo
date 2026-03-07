113c759c15b928e33b05cc577178fb8a79057d19
Guido Pastorino <96697892+guidopastorino@users.noreply.github.com>
2026-03-06 22:11:51 -0300

    feat: add Docker support (#9)

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,4 +1,8 @@
+import path from "path";
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.resolve(process.cwd(), ".env") });
 
 export default defineConfig({
   schema: "./src/db/schemas/*",
